d695f505a6f562e23d4f39fca55bffa29809769c
Babatunde <tundenasri@gmail.com>
2019-11-14 11:28:31 +0100

    make landing page responsive

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,28 +1,21 @@
 import React from "react";
-import {
-  Button,
-  FormGroup,
-  Input,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+import { Button, FormGroup, Input, Container, Row, Col } from "reactstrap";
 import "./home.css";
 import { Link } from "react-router-dom";
 
-const Home = ({handleInput, handleSubmit, loading, input}) => {
+const Home = ({ handleInput, handleSubmit, loading, input }) => {
   return (
     <div>
       <Container style={{ marginTop: "12%" }}>
         <Row>
-          <Col>
+          <Col sm="6" xs="12">
             <h1 className="headerText"> Github users listing</h1>
             <p className="descriptionText">
-              Search through github users and peep their profile :)
+              Search through github users and peep at their profile :)
             </p>
             <Row>
-              <FormGroup>
-                <Col>
+              <Col sm="8" xs="12">
+                <FormGroup className="searchInput">
                   <Input
                     type="text"
                     name="username"
@@ -31,13 +24,14 @@ const Home = ({handleInput, handleSubmit, loading, input}) => {
                     bsSize="lg"
                     onChange={handleInput}
                   />
-                </Col>
-              </FormGroup>
-              <Col>
+                </FormGroup>
+              </Col>
+              <Col sm="4" xs="12">
                 <Link to={`/search-result/${input}`}>
                   <Button
                     style={{ backgroundColor: "#34CABE", border: 0 }}
                     size="lg"
+                    className="btn-search"
                   >
                     Search
                   </Button>
@@ -45,8 +39,12 @@ const Home = ({handleInput, handleSubmit, loading, input}) => {
               </Col>
             </Row>
           </Col>
-          <Col>
-            <img src="/search.png" alt="" style={{ width: 600, height: 400 }} />
+          <Col sm="6" xs="12" className="searchImage">
+            <img
+              src="/search.png"
+              alt=""
+              style={{ width: "100%", height: 400 }}
+            />
           </Col>
         </Row>
       </Container>
